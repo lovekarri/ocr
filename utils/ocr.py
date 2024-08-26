078d9bfbd7488c043b03c16907975ed09805eb4f
郭森 <12105981+guosen001@user.noreply.gitee.com>
2024-08-26 10:24:03 +0800

    np.array 参数改为二进制字节流

--- utils/ocr.py
+++ utils/ocr.py
@@ -46,7 +46,8 @@ def ocr_image_with_path(image_path, use_angle_cls=True, lang="ch"):
 def ocr_image_from_bytes(image_bytes, use_angle_cls=True, lang="ch"):
 	# img_data = base64.b16encode(image_bytes).decode('utf-8')
 	# img_base64 = f"data:image/jpeg;base64,{img_data}"
-	temp_image = Image.open(io.BytesIO(image_bytes))
+	# temp_image = Image.open(io.BytesIO(image_bytes))
+	temp_image = image_bytes
 
 	ocr = PaddleOCR(use_angle_cls=use_angle_cls, lang=lang)
 	result = ocr.ocr(np.array(temp_image), cls=use_angle_cls)
