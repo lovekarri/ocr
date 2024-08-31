b97b103fb1bb55a07aef32cb1e1d587b8bbaa83b
郭森 <12105981+guosen001@user.noreply.gitee.com>
2024-08-31 20:38:34 +0800

    重构API:image-ocr的代码,添加异步保存结果json文件

--- app/ocr.py
+++ app/ocr.py
@@ -1,8 +1,9 @@
 import os
 import asyncio
 import aiofiles
+import json
 
-from fastapi import HTTPException
+from fastapi import HTTPException, UploadFile, File
 from PIL import Image
 
 from ocr.angle import closed_angle_of_result
@@ -26,12 +27,12 @@ class OCR:
         pass
 
 
-# filename: 原始文件名
-# anglevalue: 旋转的角度，如果为-1则为该图片未识别成功，未旋转
+# file_name: 原始文件名
+# angle_value: 旋转的角度，如果为-1则为该图片未识别成功，未旋转
 # clockwise: 1为顺时针旋转，-1为逆时针旋转
-# original: 原图ocr识别结果
-# rotated: 原图旋转anglevalue角度后重新ocr识别的结果
-def final_result(code, file_name, angle_value, clock_wise, original_result, rotated_result):
+# original_result: 原图ocr识别结果
+# rotated_result: 原图旋转anglevalue角度后重新ocr识别的结果
+def final_result(code: int, file_name: str, angle_value: float, clock_wise: bool, original_result: list, rotated_result: list) -> dict:
     return {
         'status': code,
         'result': {
@@ -49,7 +50,7 @@ def count_the_real_angle_of_anticlockwise(angle_value: float) -> float:
     return angle_value if angle_value >= 0 else angle_value + 360
 
 
-# 保存图片到指定路径
+# 异步保存图片到指定路径
 async def save_image(file_path: str, image: Image) -> None:
     async with aiofiles.open(file_path, 'wb') as f:
         image.save(f, format='PNG')
@@ -60,6 +61,14 @@ def save_original_image(file_name:str, image:Image) -> None:
     full_path = os.path.join(ORIGINAL_IMAGE_DIRECTORY, file_name)
     save_image(full_path, image)
 
+
+# 异步保存json文件
+async def save_json_file(file_name: str, result: list) -> None:
+    json_file = json.load(result)
+    json_name = os.path.splitext(file_name)[0] + '.json'
+    full_path = os.path.join(OCR_JSON_SAVE_DIRECTORY, json_name)
+    async with open(full_path, 'wb', encoding='utf-8') as f:
+        json.dump(json_file, f, ensure_ascii=False, indent=4)
 
 
 # 绘制描点后保存图片
@@ -75,7 +84,7 @@ async def save_result_image(file_name: str, image: Image, result: list, angle_va
 
 
 # 获取图片ocr结果数据
-async def result_with_binary_data(binary_data, file_name):
+async def result_with_binary_data(binary_data: bytes, file_name: str) -> dict:
     # 初始化BytesIO字节串
     bytesio = bytesio_with_binary_data(binary_data)
     # 保存原图
@@ -85,15 +94,15 @@ async def result_with_binary_data(binary_data, file_name):
     origin_result = ocr_image_from_bytesio(bytesio, True, 'ch')
     # 识别结果为空，直接返回
     if len(origin_result) == 0:
-        return final_result(200, file_name, -1, -1, [], [])
+        return final_result(200, file_name, -1, False, [], [])
     # 计算最佳旋转角度
     angle_value = closed_angle_of_result(origin_result)
-    # 保存识别标注后的图片
+    # 异步保存识别标注后的图片
     asyncio.run(save_result_image(file_name, image_with_bytesio(bytesio), origin_result, angle_value))
     
     # 角度 < 1 直接返回，不进行旋转
     if abs(angle_value) < 1:
-        return final_result(200, file_name, angle_value, -1, origin_result, [])
+        return final_result(200, file_name, angle_value, False, origin_result, [])
     # 计算实际旋转角度
     anticlosewise = count_the_real_angle_of_anticlockwise(angle_value)
     # 旋转图片
@@ -105,14 +114,18 @@ async def result_with_binary_data(binary_data, file_name):
 
     # 旋转后的图片名称
     rotated_file_name = os.path.splitext(file_name)[0] + '_rotated.png'
-    # 保存第二次识别标注后的图片
+    # 异步保存第二次识别标注后的图片
     asyncio.run(save_result_image(rotated_file_name, rotated_image, rotated_result))
 
-    return final_result(200, file_name, anticlosewise, -1, origin_result, rotated_result)
+    final = final_result(200, file_name, anticlosewise, False, origin_result, rotated_result)
+    # 保存最终json文件
+    save_json_file(file_name, final)
+
+    return final
 
 
 # 获取图片识别结果
-async def response_data_from_body(file):
+async def response_data_from_body(file: UploadFile = File(...)) -> dict:
     if not file.content_type:
         raise HTTPException(status_code=400, detail="未提供文件")
 
