301157bd3dae594c8797c97f938534a54d182613
郭森 <12105981+guosen001@user.noreply.gitee.com>
2024-09-01 22:20:17 +0800

    修改detection api的文件名

--- app/detection.py
+++ app/detection.py
@@ -16,23 +16,23 @@ DETECTION_PATH = "/paddle/PaddleDetection/"
 os.makedirs(DETECTION_SAVE_DIRECTORY, exist_ok=True)
 
 # 用于存储内存中的文件对象
-memory_files = {}
+# memory_files = {}
 
-def get_image():
-	if memory_files == {}:
-		return None
-	else:
-		return memory_files
+# def get_image():
+# 	if memory_files == {}:
+# 		return None
+# 	else:
+# 		return memory_files
 
 
 
-def result_with_bytesio(bytesio: io.BytesIO, file_name: str) -> dict:
+# def result_with_bytesio(bytesio: io.BytesIO, file_name: str) -> dict:
 
 
 
 # 获取图片识别结果
-async def response_data_from_body(file: UploadFile = File(...)) -> dict:
+# async def response_data_from_body(file: UploadFile = File(...)) -> dict:
 
-    memory_files[filename] = binary_data
+#     memory_files[filename] = binary_data
     
-    return {}
+#     return {}
