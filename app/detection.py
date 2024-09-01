65f05181beb947e2b524f64396c4704b7dc62084
郭森 <12105981+guosen001@user.noreply.gitee.com>
2024-09-01 23:00:23 +0800

    API image-detection 修改逻辑

--- app/detection.py
+++ app/detection.py
@@ -18,15 +18,13 @@ DETECTION_PATH = "/paddle/PaddleDetection/"
 os.makedirs(DETECTION_SAVE_DIRECTORY, exist_ok=True)
 
 
-
-def result_with_binary_data(binary_data: bytes, file_name: str) -> dict:
-    file_path = os.path.join(DETECTION_SAVE_DIRECTORY, file_name)
-    print(f'file_path = {file_path}')
-
-    # 将上传的文件保存到指定目录
-    with open(file_path, "wb") as f:
+# 将文件保存到指定目录
+def save_image(file_path: str, binary_data: bytes) -> None:
+    with open(file_path, 'wb') as f:
         f.write(binary_data)
 
+
+def detection(file_path: str):
     img_file = "--image_file=" + file_path
     infer_file = DETECTION_PATH + "deploy/python/infer.py"
     # "--model_dir=./output_inference/picodet_lcnet_x2_5_640_mainbody"
@@ -40,18 +38,14 @@ def result_with_binary_data(binary_data: bytes, file_name: str) -> dict:
 
     # 使用subprocess.run执行命令
     result = subprocess.run(command, capture_output=True, text=True)
+    return result
 
-    # 获取命令的标准输出和标准错误
-    stdout = result.stdout
-    stderr = result.stderr
 
-    # 打印输出和错误（可选）
-    print("STDOUT:", stdout)
-    print("STDERR:", stderr)
-
+def results_with_re(result) -> list:
     pattern = r"class_id:(\d+), confidence:([0-9.]+), left_top:\[([0-9.]+),([0-9.]+)\],right_bottom:\[([0-9.]+),([0-9.]+)\]"
     # 使用正则表达式查找所有匹配项
-    matches = re.findall(pattern, stdout)
+    matches = re.findall(pattern, result)
+
     # 将匹配项组装成字典列表
     results = [
         {
@@ -64,6 +58,27 @@ def result_with_binary_data(binary_data: bytes, file_name: str) -> dict:
         }
         for class_id, confidence, x, y, w, h in matches
     ]
+    return results
+
+
+def result_with_binary_data(binary_data: bytes, file_name: str) -> dict:
+    file_path = os.path.join(DETECTION_SAVE_DIRECTORY, file_name)
+    print(f'file_path = {file_path}')
+
+    # 将上传的文件保存到指定目录
+    save_image(file_path, binary_data)
+
+    result = detection(file_path)
+
+    # 获取命令的标准输出和标准错误
+    stdout = result.stdout
+    stderr = result.stderr
+
+    # 打印输出和错误（可选）
+    print("STDOUT:", stdout)
+    print("STDERR:", stderr)
+
+    results = results_with_re(result.stdout)
 
     # 检查命令是否成功执行
     if result.returncode == 0:
