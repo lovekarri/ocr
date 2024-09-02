3a0256fd4bb586773de0f2e670b50e1463e6129e
郭森 <12105981+guosen001@user.noreply.gitee.com>
2024-09-02 19:29:04 +0800

    API image-detection 修改逻辑

--- main.py
+++ main.py
@@ -142,7 +142,6 @@ async def upload_binary_data(file: UploadFile = File(...)):
         infer_file,
         model_dir,
         img_file,
-        '--output_dir ./output/valid'
     ]
 
     # 使用subprocess.run执行命令
